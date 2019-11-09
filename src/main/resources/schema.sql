2ecaa23966f1e2770f6855bb3c87edfaac8cdb74
harshavardhan2712 <57551185+harshavardhan2712@users.noreply.github.com>
2019-11-09 12:15:22 +0530

    Data Models/Tables required for services

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,40 @@ create table user
    password varchar2(255) not null,
    primary key(username)
 );
+
+
+CREATE TABLE DBS_PORTFOLIO_TRANSACTION
+(
+  ExchangeName      varchar2(32) NOT NULL,
+  ExternalSourceId  varchar2(64) NOT NULL,
+  ISIN 				varchar2(32),
+  StockName 		varchar2(255),
+  Quantity          Number,
+  Operation			varchar2(32),
+  Price				Double,
+  IsIntraDay        Integer,
+  SourceSystem      varchar2(64),
+  CreationDate      timestamp,
+  Currency          varchar2(32),
+  RowId             Number,  
+);
+
+
+CREATE TABLE DBS_PORTFOLIO_SUMMARY
+(
+   ExchangeName 		varchar2(32),
+   ISIN         		varchar2(32),
+   StockName    		varchar2(64),
+   AverageCMP   		Double,
+   RealizedProfit/Loss  Double,
+   UnRealizedProfit/Loss Double,
+   CurrentCMP			Double
+   );
+   
+   CREATE TABLE FX_EXCHANGE
+   (
+     SourceCurrency   varchar2(64),
+     TargetCurrrency  varchar2(64),
+     ExchangeValue    Double
+     );
+   
